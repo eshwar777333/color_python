c8a7f458dae8d0a40d21694113c43eccfb9b12ab
Eshwar chowdhary <75072446+eshwarchowdhary@users.noreply.github.com>
2021-04-29 12:27:32 +0530

    init

---
+++ settu.py
@@ -0,0 +1,12 @@
+import turtle
+col=('red','yellow','green','blue','orange','white')
+t=turtle.Turtle()
+screen=turtle.Screen()
+screen.bgcolor('black')
+t.speed(105)
+for i in range (520):
+
+ t.color(col[i%6])
+ t.forward(i*1.5)
+ t.left(59)
+ t.width(5)
